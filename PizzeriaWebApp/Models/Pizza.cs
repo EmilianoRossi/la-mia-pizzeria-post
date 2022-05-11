b6c3380d71689f707349f9503703ff118376716e
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-05-11 12:40:32 +0200

    Required per validazione inserito

--- PizzeriaWebApp/Models/Pizza.cs
+++ PizzeriaWebApp/Models/Pizza.cs
@@ -10,7 +10,7 @@ namespace PizzeriaWebApp.Models
         public string nome { get; set; }
         [Required(ErrorMessage = "Il prezzo è obbligatorio")]
         public double prezzo { get; set; }
-        [Required(ErrorMessage = "descrizione è obbligatoria")]
+        [Required(ErrorMessage = "descrizione è obbligatorio")]
         public string descrizione { get; set; }
         [Required(ErrorMessage = " Il link della foto è obbligatorio")]
         [Url(ErrorMessage ="Devi inserire un Url")]
